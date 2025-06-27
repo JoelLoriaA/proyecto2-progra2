6d01c3ac5a9398aa7b1d675baef5f219e27341d0
EmaAraya <emanuelarayasanabria2@gmail.com>
2025-06-27 12:25:01 -0600

    estructura lista

---
+++ src/main/java/cr/ac/ucr/servicarpro/proyecto2/progra2/servicies/ClienteService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.ucr.servicarpro.proyecto2.progra2.servicies;
+
+/**
+ *
+ * @author Emanuel Araya
+ */
+public class ClienteService {
+    
+}
