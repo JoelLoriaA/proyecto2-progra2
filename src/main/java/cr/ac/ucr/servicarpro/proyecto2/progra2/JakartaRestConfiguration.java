0f84c66bb8c7121c064c63e2cae5a9136f4daa91
JoelLoríaA <joel.loria@ucr.ac.cr>
2025-06-27 11:16:11 -0600

    creación de proyecto

---
+++ src/main/java/cr/ac/ucr/servicarpro/proyecto2/progra2/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.servicarpro.proyecto2.progra2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
